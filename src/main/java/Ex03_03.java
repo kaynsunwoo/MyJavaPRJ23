3117a012605bda7f98410975cb30f1e7ba03b625
Parksunwoo <kaynsunwoo@naver.com>
2022-03-23 11:43:11 +0900

    20220323

---
+++ src/main/java/Ex03_03.java
@@ -0,0 +1,9 @@
+public class Ex03_03 {
+
+    public static void main(String[] args) {
+
+        System.out .printf("%d /%d = %d" ,100,200,1);
+
+
+    }
+}
---
+++ src/main/java/Exam01.java
@@ -0,0 +1,11 @@
+public class Exam01 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("박선우");
+
+    }
+
+
+}
